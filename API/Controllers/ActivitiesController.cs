13c6f943d2265f8070c9c160bb672ebfcb4e7d96
Kun Ma <kma126@sfu.ca>
2023-03-11 16:53:22 -0800

    Half complete of skelton

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,24 @@
+using Persistence;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    public class ActivitiesController : BaseApiController
+    {
+        private readonly DataContext _context;
+        public ActivitiesController(DataContext context)
+        {
+            _context = context;            
+        }
+        [HttpGet] //apt/activites
+        public async Task<ActionResult<List<Activity>>> GetActivities(){
+            return await _context.Activities.ToListAsync();
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Activity>> GetActivity(Guid id){
+            return await _context.Activities.FindAsync(id);
+        }
+    }
+}
